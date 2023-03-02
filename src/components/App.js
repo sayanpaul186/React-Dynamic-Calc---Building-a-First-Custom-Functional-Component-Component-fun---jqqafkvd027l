97ba6a6fde0faaafeb42fba680d23a82b75062b9
sayanpaul186 <116382826+sayanpaul186@users.noreply.github.com>
2023-03-02 11:58:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,32 @@
-import React,{useState, useEffect} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import Input from "./Input";
+import "./styles.css";
 
-function App() {
-  const [currentSum,setCurrentSum]=useState(0);
-  const [clear,setClear]=useState(false);
+const App = () => {
+  const [count, setCount] = useState("");
+  const [count2, setCount2] = useState("");
 
-  useEffect(()=>{
-    document.querySelector('#result').value="";
-  },[])
-  
-  useEffect(()=>{
-    if(clear)
-    document.querySelector('#result').value="";
-  })
+  const handleaddNumber = (e) => {
+    setCount(e.target.value);
+  };
 
-  const Add=(e)=>{
-    e.preventDefault();
-    if(clear) setClear(false);
-    let currentNum=document.querySelector('#num').value
-    if(currentNum=='')
-    return;
-    let sum= currentSum+parseInt(currentNum);
-    setCurrentSum(sum);
-    document.querySelector('#num').value="";
-      
-  }
+  const handleaddNumber2 = (e) => {
+    setCount2(e.target.value);
+  };
 
-  const Clear=(e)=>{
-    e.preventDefault();
-    console.log('sum:', currentSum);
-    document.querySelector('form').reset();
-    setClear(true);
-    setCurrentSum(0);
+  function resetValFunc() {
+    setCount("");
+    setCount2("");
   }
 
   return (
     <div className="App">
-      <div className="app-title">
-        <h1> Basic Form Calculator</h1>
-      </div>
-      <form>
-            <input type="text" id="result" value={currentSum}  readOnly />   
-            <input type="text" id="num" placeholder="enter a number" />
-            <button onClick={Add}>Add</button>
-            <button onClick={Clear}>Clear</button>
-      </form>
+      <h2>add numbers</h2>
+      <input onChange={handleaddNumber} value={count} type="number" />
+      <input onChange={handleaddNumber2} value={count2} type="number" />
+      <Input values={count} values2={count2} resetVal={resetValFunc} />
     </div>
   );
-}
+};
 
 export default App;
