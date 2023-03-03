fd183479a4215e475c3e78fa9191bb603348f75e
sayanpaul186 <116382826+sayanpaul186@users.noreply.github.com>
2023-03-03 20:58:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,30 @@
-import React, { useState } from "react";
-import Input from "./Input";
-import "./styles.css";
+import React,{useState} from 'react'
+import '../styles/App.css';
 
 const App = () => {
-  const [count, setCount] = useState("");
-  const [count2, setCount2] = useState("");
-
-  const handleaddNumber = (e) => {
-    setCount(e.target.value);
-  };
-
-  const handleaddNumber2 = (e) => {
-    setCount2(e.target.value);
-  };
-
-  function resetValFunc() {
-    setCount("");
-    setCount2("");
+  const [expense, setExpense] = useState(0);
+  const handle = () => {
+    const a = document.createElement("li");
+    const b = document.getElementById("expense-input").value;
+    a.innerHTML = b;
+    document.getElementById("expense-list").appendChild(a);
+    const c = b.indexOf('-');  
+    const d = b.slice(c + 1).trim();
+    setExpense(parseInt(d) + expense);
   }
 
   return (
-    <div className="App">
-      <h2>add numbers</h2>
-      <input onChange={handleaddNumber} value={count} id="input1" type="number" />
-      <input onChange={handleaddNumber2} value={count2} id="input2" type="number" />
-      <Input values={count} values2={count2} id="result" resetVal={resetValFunc} />
+    <div id="main">
+      <input id="expense-input" />
+      <button id="expense-button" onClick={handle}>Add Expense</button>
+      <div id="expense-list">
+      </div>
+      <div id="total-expense">
+        Total Expense: 400
+      </div>
     </div>
-  );
-};
+  )
+}
+
 
 export default App;
